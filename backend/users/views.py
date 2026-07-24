64b9b31184b2f1669fb2ff64f026d8069b0ac9ce
Don Soby <donkiran2255@gmail.com>
2026-07-24 21:57:08 +0530

    feat: implement user registration and logout views with JWT token handling

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .serializers import UserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(
+                {
+                    "message": "User registered successfully.",
+                    "user": serializer.data,
+                },
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutView(APIView):
+    def post(self, request):
+
+        refresh_token = request.data.get("refresh_token")
+
+        token = RefreshToken(refresh_token)
+        token.blacklist()
+
+        return Response({"message": "User logged out successfully."}, status=status.HTTP_200_OK)
