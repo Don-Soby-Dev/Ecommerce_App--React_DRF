69b0ddbbf0b3214c3a1dffa9b9e538876babcb6c
Don Soby <donkiran2255@gmail.com>
2026-07-24 21:10:24 +0530

    feat: add users app with models, admin, and initial configuration

---
+++ backend/users/models.py
@@ -0,0 +1,20 @@
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# User model extends the AbstrcactUser model to add additional fields and functionality
+
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True)
+    is_active = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.username
