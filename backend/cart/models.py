af065be9c6cbe044a21f6d17e0a7642b59ccce51
Don Soby <donkiran2255@gmail.com>
2026-07-26 15:03:01 +0530

    feat: add cart app with models, migrations, and initial setup

---
+++ backend/cart/models.py
@@ -0,0 +1,29 @@
+import uuid
+
+from django.db import models
+
+from products.models import Product
+from users.models import User
+
+
+# Cart model
+class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="cart")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class CartItem(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="cart_items"
+    )
+    quantity = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ("cart", "product")
