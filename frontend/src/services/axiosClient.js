c49f1d5d7336b5ec61f5cfd7b5f4ab3bae3941eb
Don Soby <donkiran2255@gmail.com>
2026-08-04 11:16:43 +0530

    refactor: migrate auth state to Redux and implement automatic token refresh interceptor while removing unused constants

--- frontend/src/services/axiosClient.js
+++ frontend/src/services/axiosClient.js
@@ -1,13 +1,16 @@
 import axios from "axios";
-import { ACCESS_TOKEN } from "../constants";
+import { store } from "../app/store"
+import { setCredentials, logOut } from "../features/auth/authSlice";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
 
 api.interceptors.request.use(
   (confg) => {
-    const token = localStorage.getItem(ACCESS_TOKEN);
+    const token = store.getState().auth.accessToken;
+
     if (token) {
       confg.headers.Authorization = `Bearer ${token}`;
     }
@@ -17,6 +20,77 @@ api.interceptors.request.use(
   (error) => {
     return Promise.reject(error);
   },
+);
+
+let isRefreshing = false
+let failedQueue = []
+
+const processQueue = (error, token = null) => {
+  failedQueue.forEach(promise => {
+    if (error) {
+      promise.reject(error);
+    } else {
+      promise.resolve(token);
+    }
+  })
+  failedQueue = []
+}
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      
+      if(originalRequest.url.includes('/api/token/refresh/') || originalRequest.url.includes('/api/auth/login/')) {
+        return Promise.reject(error);
+      }
+
+      if (isRefreshing){
+        return new Promise((resolve, reject) => {
+          failedQueue.push({resolve, reject})
+        }).then((token) => {
+          originalRequest.headers.Authorization = `Bearer ${token}`;
+          return api(originalRequest)
+        }).catch((err) => {
+          return Promise.reject(err);
+        })
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true;
+
+      try{
+
+        const response = await api.post('/api/token/refresh/')
+
+        const newAccessToken = response.data.data.access_token
+
+        store.dispatch(setCredentials({accessToken: newAccessToken}))
+
+        processQueue(null, newAccessToken)
+        
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        return api(originalRequest);
+
+    }catch(error){
+
+      processQueue(error, null);
+
+      store.dispatch(logOut());
+
+      return Promise.reject(error);
+
+    }finally{
+      isRefreshing = false;
+    }
+  }
+    return Promise.reject(error);
+  }
 );
 
 export default api;
